b952e0be1bcde7c41be657668ffda33a5688cd40
Nadja <60919109+NadjaKry@users.noreply.github.com>
2020-02-20 17:05:50 +0100

    Update 1_Download.sh

--- Scripts/1_Download.sh
+++ Scripts/1_Download.sh
@@ -6,11 +6,11 @@ echo -e "\n***"
 mkdir Containers
 cd Containers
 
-singularity pull library://nadjakry/default/spades:3.14.0
-singularity pull library://nadjakry/default/quast:5.0.2
-singularity pull library://nadjakry/default/multiqc:1.6
-singularity pull library://nadjakry/default/fastqc:0.11.8
-singularity pull library://nadjakry/default/checkm:1.1.2
+singularity pull -U library://nadjakry/default/spades:3.14.0
+singularity pull -U library://nadjakry/default/quast:5.0.2
+singularity pull -U library://nadjakry/default/multiqc:1.6
+singularity pull -U library://nadjakry/default/fastqc:0.11.8
+singularity pull -U library://nadjakry/default/checkm:1.1.2
 
 cd ..
 
